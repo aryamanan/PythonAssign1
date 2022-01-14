e75bf67ecb35a52a063e8ec3d016b9ac5b0e1c73
Aryan Arora <aryanarora190402@gmail.com>
2022-01-14 21:58:51 +0530

    Add files via upload

---
+++ Output.py
@@ -0,0 +1,51 @@
+Welcome to my Assignment1 [Copyright Aryan Arora(21103007), all rights reserved]
+
+
+Run programs from Ques1 to Ques5b in order [Y or N]: y
+
+
+
+Question 1 program
+
+enter marks of student 1: 92
+enter marks of student 2: 83
+enter marks of student 3: 77
+The average of 92.0 83.0 77.0 is 84.0
+
+Press R to proceed: r
+Question 2 program
+
+Enter your gross income in dollars: 200000
+Enter total dependents: 3
+Your income tax is:  36200.0
+
+Press R to proceed: r
+Question 3 program
+
+Enter SID: 2103007
+Enter name: Aryan Arora
+Enter Gender (M, F or U for unknown): M
+Enter course name: CSE
+Enter CGPA: 9
+['2103007', 'Aryan Arora', 'M', 'CSE', 9.0]
+
+Press R to proceed: r
+Question 4 program
+
+Enter mark of Student:76
+Enter mark of Student:44
+Enter mark of Student:95
+Enter mark of Student:58
+Enter mark of Student:31
+[31.0, 44.0, 58.0, 76.0, 95.0]
+
+Press R to proceed: r
+Question 5a program
+
+['red', 'Green', 'White', 'Pink', 'Yellow']
+
+Press R to proceed: r
+Question 5b program
+
+['red', 'Green', 'White', 'Purple', 'Purple', 'Yellow']
+press R to restart and any other key to end program:
